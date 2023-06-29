6ff153631d2c85189370a857249b2c4289435e3a
agfebrian <febriananggi9@gmail.com>
2023-06-29 17:22:48 +0700

    Add font awesome icon

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,15 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./routes";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faEye, faEyeSlash);
+
 const app = createApp(App);
 
 app.use(router);
+app.component("FontAwesomeIcon", FontAwesomeIcon);
+
 app.mount("#app");
