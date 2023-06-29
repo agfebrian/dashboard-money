90838752641c2fe659bb1b2492e1875f2936f062
agfebrian <febriananggi9@gmail.com>
2023-06-29 16:23:53 +0700

    Add router

---
+++ src/routes/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      name: "login",
+      path: "/login",
+      component: import("@/pages/LoginPage.vue"),
+      meta: {
+        layout: "blank",
+      },
+    },
+  ],
+});
+
+export default router;
