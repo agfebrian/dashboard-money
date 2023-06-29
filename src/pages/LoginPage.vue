90adbc8f885dda9bc2e31f8184c5dac69cae85ed
agfebrian <febriananggi9@gmail.com>
2023-06-29 16:28:02 +0700

    Create login page

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex h-screen w-full items-center justify-center bg-slate-300">
+    <form action="#" autocomplete="off">
+      <div class="flex w-[500px] flex-col gap-3 rounded-md bg-white p-4">
+        <h2 class="mb-2 text-center text-2xl font-bold">Login</h2>
+        <div class="flex flex-col">
+          <label for="email" class="mb-1 text-base text-slate-400">
+            Email
+          </label>
+          <app-input id="email" placeholder="Email" />
+        </div>
+        <div class="flex flex-col">
+          <label for="password" class="mb-1 text-base text-slate-400">
+            Password
+          </label>
+          <app-input id="password" type="password" placeholder="Password" />
+        </div>
+        <app-button> Submit </app-button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import AppInput from "@/components/ui/input/AppInput.vue";
+import AppButton from "@/components/ui/buttons/AppButton.vue";
+</script>
